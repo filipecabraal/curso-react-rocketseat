9dc6f92bc5bf91e1634b0d25630243f9102b1e80
Filipe Cabral <filipecabraal@gmail.com>
2024-04-16 10:31:05 -0300

    [Modulo 02] -> Começando a construir o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import styles from './Header.module.css';
 
+import Logo from '../assets/logo.svg';
+
 // Criar comp. React com a primeira letra maiúscula, p/ não conflitar com palavras chaves;
 export  function Header() {
     return (
         <>
             <header className={styles.header}>
-                <strong>Ignite Feed</strong>
+                <img src={Logo} alt="Logotipo curso React Rocket Seat" />
             </header>
         </>
     );

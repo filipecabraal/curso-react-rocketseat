e37ce1168862e79ba3961dda22c6333a483c26a8
Filipe Cabral <filipecabraal@gmail.com>
2024-05-07 10:58:01 -0300

    [Modulo 03] -> Validação do campo de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,6 +14,7 @@ export function Post({ author, publishedAt, content }) {
 
     const [newCommentText, setNewCommentText] = useState('');
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
 
@@ -27,6 +28,10 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText('');
     }
 
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+    }
+
     function deleteComment(commentToDelete) {
         const newComments = comments.filter(item => {
             return item !== commentToDelete
@@ -34,6 +39,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(newComments);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -66,10 +73,14 @@ export function Post({ author, publishedAt, content }) {
                     placeholder='Deixe um comentário'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
